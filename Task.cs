75dfffcc293c1c84b8fd297360d82c817f4d028b
Lewis <88146791+lafexx@users.noreply.github.com>
2022-05-13 13:44:51 +1000

    added major functionality

--- Task.cs
+++ Task.cs
@@ -1,8 +1,24 @@
 
 public class Task : Monobehaviour
 {
+    public Object Task_Target;
     public string Attached_Function;
     public bool Task_Complete;
+    public string Task_Name;
+
+    public void Start()
+    {
+        if (Task_Target.Invoke(Attached_Function, 0.0f))
+        {
+            Task_Complete = true;   
+        }
+        else
+        {
+            Task_Complete = true; // this is a temporary solution
+            Debug.Log("Task failed to complete adding to the retry list");
+            // TODO: Add to retry list
+        }
+    }
 
     public static Task Return_Task_Information()
     {
