66f97a8360419c6d427924955b69bef696132f4b
Lewis <88146791+lafexx@users.noreply.github.com>
2022-05-13 13:44:22 +1000

    added more examples / documentation

--- Example.cs
+++ Example.cs
@@ -6,11 +6,28 @@ public class Example : Monobehaviour
 
     private void Start()
     {
-        process_handler.TryToCreateTask("Task 1", "FUNCTION_1");
-        process_handler.TryToCreateTask("Task 2", "FUNCTION_2");
-        process_handler.WhenTasksComplete(this, "WhenTasksComplete");
+        // trying to add a task to the active process
+        process_handler.TryToCreateTask("Task 1", this, "FUNCTION_1");
+        // trying to add another task to the active process
+        process_handler.TryToCreateTask("Task 2", this, "FUNCTION_2");
+        // when the process handler completes all the tasks in the process run this function
+        process_handler.WhenTasksComplete(this, "WhenTasksComplete"); // PARAM 1 is should be the class that holds the function
     }
 
+    // when creating a function for a task it needs to be public, static and return a boolean which tells the task if the function was succesfull
+    public static bool FUNCTION_1()
+    {
+        Debug.Log("FUNCTION_1");
+        return true;
+    }
+
+    public static bool FUNCTION_2()
+    {
+        Debug.Log("FUNCTION_2");
+        return true;
+    }
+
+    // this is the function that we set to run when the process handler completes all the tasks
     public static void WhenTasksComplete()
     {
         Debug.Log("All the tasks have been completed");
